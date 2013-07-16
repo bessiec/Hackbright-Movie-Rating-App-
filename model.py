71fb6ce8db80ba4fd8d2a0c4d93a96471026619e
Hackbright Student <amy.r.lam@gmail.com>
2013-07-15 18:04:45 -0700

    initial commit

--- model.py
+++ model.py
@@ -1,13 +1,53 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String #importing Datetime here didn't work. WHY?
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
 
+class User(Base):
+    __tablename__ ="users"
+
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(65), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+    gender = Column(String(3), nullable=True)
+    profession = Column(String(70), nullable=True)
+
+class Movies(Base):
+    __tablename__="movies"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(64), nullable=True)
+    released_at = Column(String(80), nullable=True) #we need to convert/import Datetime later 
+    imdb_url = Column(String(120), nullable=True)
+
+
+class Ratings(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer, nullable = True)
+    user_id = Column(Integer, nullable = True)
+    rating = Column(Integer, nullable = True) # can only be from 1-5
 
 ### End class declarations
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -2,8 +2,13 @@ import model
 import csv
 
 def load_users(session):
-    # use u.user
-    pass
+    with open ('seed_data/u.user','rb') as f:
+        filename_users = csv.reader(f, delimiter='|')
+        for user_row in filename_users:
+            new_user_row = User(id=user_row[0],age=user_row[1],gender=user_row[2], profession=user_row[3], zipcode=user_row[4])
+            session.add(new_user_row)
+        session.commit()   
+    #print filename_users
 
 def load_movies(session):
     # use u.item
@@ -15,6 +20,7 @@ def load_ratings(session):
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
+    #load_users(session)
     pass
 
 if __name__ == "__main__":
